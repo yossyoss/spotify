dde8f749de7b3ba14db1c3cdb7141708cd3d398b
yossi <yossi@hiredscore.com>
2022-12-06 23:25:36 +0200

    adding-prisma

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,5 @@
 import NextImage from 'next/image'
+import NextLink from 'next/link'
 import {
     Box,
     List,
@@ -35,6 +36,19 @@ const navMenu = [
     },
 ]
   
+const musicMenu = [
+  {
+    name: 'Create Playlist',
+    icon: MdPlaylistAdd,
+    route: '/',
+  },
+  {
+    name: 'Favorites',
+    icon: MdFavorite,
+    route: '/favorites',
+  },
+]
+const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`)
 const SideBar = () => {
     return (
     <Box
@@ -48,6 +62,62 @@ const SideBar = () => {
           <Box width="120px" marginBottom="20px" paddingX="20px">
             <NextImage src="/yossify.svg" height={60} width={120} />
           </Box>
+          <Box marginBottom="20px">
+            <List spacing={2}>
+              {navMenu.map(menu => (
+                <ListItem paddingX="20px" fontSize="16px" key="menu.name">
+                  <LinkBox>
+                    <NextLink href={menu.route} passHref>
+                      <LinkOverlay>
+                        <ListIcon as={menu.icon} color="white" marginRight="20px" />
+                        {menu.name}
+                      </LinkOverlay>
+                    </NextLink>
+                    </LinkBox>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+          <Box marginTop="20px">
+          <List spacing={2}>
+            {musicMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+          </Box>
+          <Divider color="gray.800" />
+          <Box height="66%" overflowY="auto" paddingY="20px">
+          <List spacing={2}>
+            {playlists.map((playlist) => (
+              <ListItem paddingX="20px" key={playlist.id}>
+                <LinkBox>
+                  <NextLink
+                    href={{
+                      pathname: '/playlist/[id]',
+                      query: { id: playlist.id },
+                    }}
+                    passHref
+                  >
+                    <LinkOverlay>{playlist.name}</LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
         </Box>
     </Box>
     )
